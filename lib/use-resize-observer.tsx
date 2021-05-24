76474a0464030b01a65db91709c0ff68a71cd86e
lucguillemot <luc.guillemot@gmail.com>
2021-05-24 09:18:56 +0200

    feat: add resize-observer hook

---
+++ lib/use-resize-observer.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState, useRef } from "react";
+import { ResizeObserver } from "@juggle/resize-observer";
+
+export const useResizeObserver = <T extends Element>() => {
+  const roRef = useRef<ResizeObserver>();
+  const elRef = useRef<T>(null);
+  const [width, changeWidth] = useState(1);
+  const [height, changeHeight] = useState(1);
+
+  useEffect(() => {
+    if (!elRef.current) {
+      return;
+    }
+
+    if (!roRef.current) {
+      roRef.current = new ResizeObserver((entries) => {
+        if (!entries.length) {
+          return;
+        }
+
+        const entry = entries[0];
+
+        const { inlineSize: width, blockSize: height } =
+          entry.contentBoxSize[0];
+
+        changeWidth(width);
+        changeHeight(height);
+      });
+    }
+
+    roRef.current.observe(elRef.current);
+
+    return () => {
+      roRef.current?.disconnect();
+    };
+  }, []);
+
+  return [elRef, width, height] as const;
+};
