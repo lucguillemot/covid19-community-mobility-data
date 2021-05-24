2a356cf679cb27669e264eaab6c2c444c7a43453
lucguillemot <luc.guillemot@gmail.com>
2021-05-24 22:03:41 +0200

    feat: update chart title styles

--- components/chart-title.tsx
+++ components/chart-title.tsx
@@ -1,17 +1,34 @@
 import * as React from "react";
 import * as CKA from "@chakra-ui/react";
 
-export const ChartTitle = ({ title }: { title: React.ReactNode }) => (
-  <CKA.Box
-    as="figcaption"
-    sx={{
-      textTransform: "uppercase",
-      fontSize: 12,
-      fontWeight: 700,
-      maxWidth: "40em",
-      my: 4,
-    }}
-  >
-    {title}
+export const ChartTitle = ({
+  title,
+  description,
+}: {
+  title: React.ReactNode;
+  description: React.ReactNode;
+}) => (
+  <CKA.Box as="figcaption" sx={{ my: 4 }}>
+    <CKA.Box
+      sx={{
+        textTransform: "uppercase",
+        fontSize: 18,
+        fontWeight: 500,
+        maxWidth: "40em",
+      }}
+    >
+      {title}
+    </CKA.Box>
+    <CKA.Box
+      sx={{
+        textTransform: "uppercase",
+        fontSize: 11,
+        fontWeight: 500,
+        maxWidth: "40em",
+        color: "gray.500",
+      }}
+    >
+      {description}
+    </CKA.Box>
   </CKA.Box>
 );
