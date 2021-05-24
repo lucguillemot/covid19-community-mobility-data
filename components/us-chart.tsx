3ccf4e62cee55e7e21df4a873733039ca5e1e3eb
lucguillemot <luc.guillemot@gmail.com>
2021-05-24 15:29:44 +0200

    feat: add US chart

---
+++ components/us-chart.tsx
@@ -0,0 +1,51 @@
+import * as React from "react";
+import { Area } from "../charts/area/area";
+import { AreaAxisHeightLinear } from "../charts/area/area-axis-height-linear";
+import { AreaAxisTime } from "../charts/area/area-axis-width-time";
+import { AreaChart } from "../charts/area/area-chart-state-provider";
+import {
+  AreaChartContainer,
+  AreaChartSvg,
+} from "../charts/area/area-container";
+import { AreaRuleY } from "../charts/area/area-rule-y";
+import { ChartTitle } from "../components/chart-title";
+// Data
+import us_only_rolling_average from "../public/us_only_rolling_average.json";
+
+export const USChart = () => {
+  return (
+    <AreaChart
+      data={us_only_rolling_average}
+      encoding={{
+        x: "date",
+        y: "ravg",
+      }}
+      margins={{
+        top: 100,
+        right: 40,
+        bottom: 40,
+        left: 40,
+      }}
+      aspectRatio={0.3}
+    >
+      <AreaChartContainer>
+        <AreaChartSvg>
+          <AreaAxisHeightLinear />
+          <Area />
+          <AreaRuleY value={0} />
+          <AreaAxisTime />
+        </AreaChartSvg>
+        <ChartTitle
+          title={
+            <>
+              Mobility Trends for places of work in the United States, 7-day
+              rolling average of percentage difference to the zero baseline.
+              <br />
+              Feb 15, 2020 to May 9th, 2021.
+            </>
+          }
+        />
+      </AreaChartContainer>
+    </AreaChart>
+  );
+};
